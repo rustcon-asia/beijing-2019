0c3a89a32f74c6045dd6feb97611a210c872d745
Luc Perkins <lucperkins@gmail.com>
2018-12-02 23:59:51 -0800

    Add anchors for documentation headers (#9)
    Signed-off-by: lucperkins <lucperkins@gmail.com>

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,8 +5,17 @@ function navbarToggle() {
   });
 }
 
+function docsAnchors() {
+  anchors.options = {
+    icon: '#'
+  }
+
+  anchors.add('.docs-content h2, .docs-content h3, .docs-content h4');
+}
+
 $(function() {
   navbarToggle();
+  docsAnchors();
 
   console.log("Welcome to the TiKV docs!");
 });
